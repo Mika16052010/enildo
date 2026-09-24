f61e877a4bc5e71236807bf8e6f50fc1bfab377d
Mika16052010 <micaelestr@gmail.com>
2026-09-24 07:56:05 -0300

    Add files via upload

---
+++ exercicio_pratico3.py
@@ -0,0 +1,27 @@
+print("============= CADASTRO =============")
+print("cadastrar aluno: 1")
+print("alterar cadastro: 2")
+print("listar cadastros: 3")
+print("excluir cadastro: 4")
+print("sair do menu: 0")
+opcão = int(input("selecione oque deve ser feito"))
+def cadastrar():
+        nome = input("DIGITE O NOME DO ALUNO: ")
+        nota = float(input("DIGITE A NOTA DO ALUNO: "))
+
+
+
+
+
+
+
+
+
+
+while True:
+
+    if opcão == 1:
+        cadastrar()
+       
+    if opcão == 0:
+         break
